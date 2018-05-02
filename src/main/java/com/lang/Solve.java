ab454abb7a3326c50c654bcaa403e67d68daa8b0
Kevin Kerr <kkerr@triplelift.com>
2018-05-02 19:41:48 -0400

    making progress

--- src/main/java/com/lang/Solve.java
+++ src/main/java/com/lang/Solve.java
@@ -2,6 +2,9 @@ package com.lang;
 
 import java.util.Scanner;
 
+import com.lang.parse.Tokenizer;
+import com.lang.parse.Tokenizer.TokenStream;
+
 public class Solve {
 
 	public static void main(String args[]) {
@@ -9,7 +12,8 @@ public class Solve {
 		Scanner scanner = new Scanner(System.in);
 		while (scanner.hasNext()) {
 			String s = scanner.nextLine();
-			System.out.println(s);
+			TokenStream tokens = Tokenizer.tokenize(s);
+			System.out.println(tokens.toString());
 			System.out.print(" > ");
 		}
 		scanner.close();
--- src/main/java/com/lang/parse/Tokenizer.java
+++ src/main/java/com/lang/parse/Tokenizer.java
@@ -1,9 +1,14 @@
 package com.lang.parse;
 
+import java.util.List;
+
+import com.google.common.collect.Lists;
+import com.lang.parse.Tokenizer.Token.TokenType;
+
 public class Tokenizer {
 	public static class Token {
 		public enum TokenType {
-			TT_PLUS, TT_LPAREN, TT_RPAREN, TT_COLON, TT_ASTER
+			TT_PLUS, TT_LPAREN, TT_RPAREN, TT_COLON, TT_ASTER, TT_VAR;
 		}
 
 		private final TokenType type;
@@ -13,12 +18,73 @@ public class Tokenizer {
 			this.type = type;
 			this.literal = lit;
 		}
+
+		@Override
+		public String toString() {
+			return type + " : " + literal;
+		}
 	}
 
-	private final String str;
+	public static TokenStream tokenize(String ln) {
 
-	public Tokenizer(String str) {
-		this.str = str;
+		List<Token> tokens = Lists.newArrayList();
+		for (int i = 0, ii = ln.length(); i < ii; i++) {
+			char c = ln.charAt(i);
+			String lit = Character.toString(c);
+			TokenType type;
+			if (c == '+') {
+				type = TokenType.TT_PLUS;
+			} else if (c == '(') {
+				type = TokenType.TT_LPAREN;
+			} else if (c == ')') {
+				type = TokenType.TT_RPAREN;
+			} else if (c == '*') {
+				type = TokenType.TT_ASTER;
+			} else if (c == ':') {
+				type = TokenType.TT_COLON;
+			} else {
+				type = TokenType.TT_VAR;
+				i++;
+				c = ln.charAt(i);
+				while (c != ' ' && i < ii) {
+					lit += Character.toString(c);
+					i++;
+				}
+			}
+			Token t = new Token(type, lit);
+			tokens.add(t);
+
+		}
+		return new TokenStream(tokens);
+	}
+
+	public static class TokenStream {
+		private final List<Token> tokens;
+		private int ptr = 0;
+
+		public TokenStream(List<Token> tokens) {
+			this.tokens = tokens;
+		}
+
+		public boolean hasToken() {
+			return tokens.size() > ptr;
+		}
+
+		public Token getNext() {
+			Token t = tokens.get(ptr);
+			ptr++;
+			return t;
+		}
+
+		@Override
+		public String toString() {
+			String ret = "";
+			for (Token t : tokens) {
+				ret += t.toString();
+				ret += " ";
+			}
+			return ret;
+		}
 	}
 
 }
