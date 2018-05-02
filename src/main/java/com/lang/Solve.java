bdd4ff8ab6dc8099fa097038bf81164c01b3184b
Kevin Kerr <kkerr@triplelift.com>
2018-05-02 19:12:41 -0400

    cool

--- src/main/java/com/lang/Solve.java
+++ src/main/java/com/lang/Solve.java
@@ -1,5 +1,17 @@
 package com.lang;
 
+import java.util.Scanner;
+
 public class Solve {
+
+	public static void main(String args[]) {
+		System.out.print(" > ");
+		Scanner scanner = new Scanner(System.in);
+		while (scanner.hasNext()) {
+			String s = scanner.nextLine();
+			System.out.println(s);
+			System.out.print(" > ");
+		}
+	}
 
 }
