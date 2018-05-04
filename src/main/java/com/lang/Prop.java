425bd561f47e9d39fdcafaa9defdb6129fb96f83
Kevin <kkerr87@gmail.com>
2018-05-03 21:05:17 -0400

    print these things

--- src/main/java/com/lang/Prop.java
+++ src/main/java/com/lang/Prop.java
@@ -1,8 +1,10 @@
 package com.lang;
 
 import java.util.List;
+import java.util.Map;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 public class Prop {
 	private static class IDMaker {
@@ -33,6 +35,14 @@ public class Prop {
 			this.hecceity = hecceity;
 		}
 		
+		public QuantifierType getType () { 
+			return type;
+		}
+		
+		public Hecceity getHecceity () {
+			return hecceity;
+		}
+		
 	}
 	public static class AtomicProp {
 		private final String name;
@@ -42,9 +52,52 @@ public class Prop {
 			this.name = name;
 			this.hecceities = ents;
 		}
+		
+		public String getName () {
+			return name;
+		}
+		
+		public List<Hecceity> getHecceities () {
+			return hecceities;
+		}
 	}
 	
 	private List<Quantifier> prefix = Lists.newArrayList();
 	private List<AtomicProp> matrix = Lists.newArrayList();
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		Map<Hecceity,Character> h2s = Maps.newHashMap();
+		char a = 'a';
+		for (Quantifier q: prefix) {
+			if (q.getType() == QuantifierType.FORALL) {
+				sb.append("forall ");
+			}
+			else {
+				sb.append("thereis ");
+			}
+			Hecceity h = q.getHecceity();
+			if (h2s.containsKey(h)) {
+				sb.append(h2s.get(h));
+			}
+			else {
+				sb.append(a);
+				h2s.put(h, a);
+				a++;
+			}
+		}
+		sb.append(":");
+		for (AtomicProp prop: matrix) {
+			String name = prop.getName();
+			sb.append(name+"(");
+			for (Hecceity h:prop.getHecceities()) {
+				char c = h2s.get(h);
+				sb.append(c);
+				sb.append(" ");
+			}
+			sb.append(")");
+		}
+		return sb.toString();
+	}
 
 }
