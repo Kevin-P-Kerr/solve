28472653360782ef4973c2cbbced74fbf2be5ec9
Kevin Kerr <kkerr@triplelift.com>
2018-06-09 13:13:22 -0400

    Revert "getting ready for debug session"
    This reverts commit 12d7386824dae279d08c7143fd7dc1433dc4db9c.

--- src/main/java/com/lang/Interpreter.java
+++ src/main/java/com/lang/Interpreter.java
@@ -580,9 +580,6 @@ public class Interpreter {
 		}
 
 		public boolean compare(Prop p) {
-			if (p == currentHypothesis) {
-				return false;
-			}
 			if (currentHypothesis.evaluate(p)) {
 				if (entity != null) {
 					System.out.println("subcase proven");
@@ -621,8 +618,8 @@ public class Interpreter {
 			}
 			p.getPrefix().clear();
 			p.getPrefix().addAll(newPrefix);
-			currentHypothesis = removeDefects(p);
-			return currentHypothesis;
+			currentHypothesis = p;
+			return removeDefects(p);
 		}
 
 		public Prop getNextEntity() {
