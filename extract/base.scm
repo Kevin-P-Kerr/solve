10739d2ed3ed81431820ec134b98a970c4c5ad7c
Kevin Kerr <kkerr@triplelift.com>
2018-06-29 16:01:44 -0400

    some ideas

--- extract/base.scm
+++ extract/base.scm
@@ -2,4 +2,17 @@
   (cons '() (make-strong-eq-hash-table 100)))
 
 (define (set-literal! entity lit) 
-  (set-car! entity lit))  
+  (set-car! entity lit))
+
+(define (evaluate-body body)
+    (if (null? body) #f
+        (construct-relation body)))
+
+
+(define (construct-relation lol)
+    (if (null? lol) #f
+        (let ((l (car lol))
+            (rest (cdr lol)))
+            (if (and (l) (all-false? rest))
+              
+(define (relation? ))
