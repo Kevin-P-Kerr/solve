8263f751785aa076ab22837347f82b1d6928ea0c
Kevin Kerr <kkerr@triplelift.com>
2019-11-24 19:05:45 -0500

    start a new class

---
+++ src/main/java/com/lang/val/AxiomSet.java
@@ -0,0 +1,15 @@
+package com.lang.val;
+
+import java.util.List;
+
+public class AxiomSet {
+	private final List<Prop> axioms;
+
+	public AxiomSet(List<Prop> a) {
+		this.axioms = a;
+	}
+
+	public void enumerateFirstNConclusions(int i) {
+
+	}
+}
