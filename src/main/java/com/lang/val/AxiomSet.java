a632989b6518bbab0e61d4d4298d1db8f512d467
Kevin Kerr <kkerr@triplelift.com>
2019-12-05 21:49:25 -0500

    move

--- src/main/java/com/lang/val/AxiomSet.java
+++ src/main/java/com/lang/val/AxiomSet.java
@@ -89,26 +89,6 @@ public class AxiomSet {
 
 	}
 
-	private static class IntWrapper {
-		private int i;
-
-		public IntWrapper(int x) {
-			i = x;
-		}
-
-		public void decrement(int y) {
-			i = i - y;
-		}
-
-		public void increment(int y) {
-			i += y;
-		}
-
-		public int get() {
-			return i;
-		}
-	}
-
 	public ProofResult contradicts(Prop toBeProven, int order) {
 		return null;
 	}
